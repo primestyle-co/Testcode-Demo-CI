f1b6cd062d26d5da50a310bb491e518303bc71d3
AnhTung <anh.tung@primelabo.com.vn>
2022-08-22 15:43:42 +0700

    add build

---
+++ public/build/assets/Home.a3d669fb.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,b as t,o as n}from"./app.bf2c33b3.js";const r=o({setup(){return{}}});function s(a,c,p,_,f,m){return n(),t("h1",null,"Home")}var d=e(r,[["render",s]]);export{d as default};
