31b2f1ebc3cc8e918891c40fa0099a5f06449579
Ly Bao Cuong <lybaocuong@gmail.com>
2022-08-29 11:15:51 +0700

    commit

--- public/deploy.php
+++ public/deploy.php
@@ -21,7 +21,7 @@ $commands = array(
 // exec commands
 $output = '';
 foreach ($commands as $command) {
-    $tmp = shell_exec($command);
+    $tmp = exec($command);
 
     $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
     $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
