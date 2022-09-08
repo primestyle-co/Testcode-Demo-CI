98be0da4145db150400dfd47827acf1175c22029
AnhTung <anh.tung@primelabo.com.vn>
2022-09-08 18:17:10 +0700

    fix bug

--- public/build/assets/Header.4bc76690.js
+++
@@ -1,1 +0,0 @@
-import{d as e,_ as o,o as r,b as n}from"./app.bf2c33b3.js";const t=e({setup(){return{}}});function s(a,c,p,_,d,f){return r(),n("h1",null,"Demo")}var u=o(t,[["render",s]]);export{u as H};
---
+++ public/build/assets/Home.510f949a.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,a as t,o as n}from"./app.95a4fd28.js";const r=o({setup(){return{}}});function a(s,c,p,_,f,m){return n(),t("h1",null,"Home")}var d=e(r,[["render",a]]);export{d as default};
