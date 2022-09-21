1dc16be6d0fe23f2f8a7ac5d7dffaaa966bc7c3e
HoAnhTungPrimelabo <55376560+HoAnhTungPrimelabo@users.noreply.github.com>
2022-09-21 18:23:05 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-// test
+// test 1
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
