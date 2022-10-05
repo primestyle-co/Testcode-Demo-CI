5418a11cfbf2b10faad2972e2b430e8a10f45f73
Ly Bao Cuong <lybaocuong@gmail.com>
2022-10-05 10:32:44 +0700

    commit

--- public/deploy.php
+++ public/deploy.php
@@ -5,6 +5,8 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+file_put_contents('deploy_post.log', var_export($_POST, true));
+file_put_contents('deploy_header.log', var_export($_SERVER, true));
 /**
  * GIT DEPLOYMENT SCRIPT
  *
